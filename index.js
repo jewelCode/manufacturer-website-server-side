9f37633d36f15ed94d94c23ede8152bb3ae28e3b
jewelCode <jewelbarua101@gmail.com>
2022-05-26 00:15:46 +0600

    review api is created and updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+var jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,13 +20,56 @@ async function run() {
   try {
     await client.connect();
     const productsCollection = client.db('manufacturer_website').collection('products');
+    const usersCollection = client.db('manufacturer_website').collection('users');
+    const reviewsCollection = client.db('manufacturer_website').collection('reviews');
 
     // Get All the products data from mongodb server
-    app.get('/product', async(req, res) =>{
-        const query = {};
-        const cursor = productsCollection.find(query);
-        const products = await cursor.toArray();
-        res.send(products);
+    app.get('/product', async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    })
+
+    // Load Single Prodcut from database
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productsCollection.findOne(query);
+      res.send(product);
+    })
+
+    // Users
+
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ result, token });
+    })
+
+    // Review Collection
+
+
+    // Post Reviews
+    app.post('/review', async (req, res) => {
+        const addNewReview = req.body;
+        const result = await reviewsCollection.insertOne(addNewReview);
+        res.send(result);
+    })
+    // Get Reviews API in the UI
+    
+    app.get('/review', async (req, res) => {
+      const query = {};
+      const cursor = reviewsCollection.find(query);
+      const reviews= await cursor.toArray();
+      res.send(reviews);
     })
   }
   finally {
