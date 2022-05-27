e87e03f92cd8fab77b6f506966d3a6673c30bdbf
jewelCode <jewelbarua101@gmail.com>
2022-05-28 00:53:57 +0600

    project api is updated

--- index.js
+++ index.js
@@ -16,12 +16,27 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ messege: 'UnAuthorized Access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ messege: 'forbidden access' })
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 async function run() {
   try {
     await client.connect();
     const productsCollection = client.db('manufacturer_website').collection('products');
     const usersCollection = client.db('manufacturer_website').collection('users');
     const reviewsCollection = client.db('manufacturer_website').collection('reviews');
+    const orderCollection = client.db('manufacturer_website').collection('orders');
 
     // Get All the products data from mongodb server
     app.get('/product', async (req, res) => {
@@ -39,6 +54,34 @@ async function run() {
       res.send(product);
     })
 
+    // User Admin Role
+
+    app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await usersCollection.findOne({ email: requester });
+      if (requesterAccount.role === 'admin') {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: 'admin' },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      }
+      else {
+        res.status(403).send({ messege: 'forbidden' });
+      }
+
+    })
+
+    app.get('/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = await usersCollection.findOne({ email: email });
+      const isAdmin = user.role === 'admin';
+      res.send({ admin: isAdmin });
+    })
+
+
     // Users
 
     app.put('/user/:email', async (req, res) => {
@@ -54,23 +97,63 @@ async function run() {
       res.send({ result, token });
     })
 
+
+    app.get('/user', async (req, res) => {
+      const users = await usersCollection.find().toArray();
+      res.send(users);
+    })
+
     // Review Collection
 
 
     // Post Reviews
     app.post('/review', async (req, res) => {
-        const addNewReview = req.body;
-        const result = await reviewsCollection.insertOne(addNewReview);
-        res.send(result);
+      const addNewReview = req.body;
+      const result = await reviewsCollection.insertOne(addNewReview);
+      res.send(result);
     })
     // Get Reviews API in the UI
-    
+
     app.get('/review', async (req, res) => {
       const query = {};
       const cursor = reviewsCollection.find(query);
-      const reviews= await cursor.toArray();
+      const reviews = await cursor.toArray();
       res.send(reviews);
     })
+
+    // Order Post
+
+    app.post('/order', async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    })
+
+    app.get('/order', async (req, res) => {
+      const orders = await orderCollection.find().toArray();
+      res.send(orders);
+    })
+
+
+    // Delete order
+    app.delete('/order/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // get individual order
+    app.get('/order', async (req, res) => {
+      const useremail = req.query.useremail;
+      const query = { useremail: useremail }
+      const orders = await orderCollection.find(query).toArray();
+      res.send(orders);
+    })
+
+
+
+
   }
   finally {
 
